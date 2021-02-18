586d9dbf8f6dc3da663f5d346bda0eef19eb26c3
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-17 19:11:02 -0500

    added more var and func

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,38 @@ var generateBtn = document.querySelector("#generate");
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 var lower = "abcdefghijklmnopqrstuvwxyz"; 
 var numerics = "0123456789"; 
-var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; 
+var special = "!@#$%^&*(){}[]=<>/,.|~?"; 
+
+var len = 8; 
+
+const combine = {
+  upper: getrandomUpper,
+  lower: getrandomLower,
+  numerics: getrandomNumber,
+  special: getrandomSpecial,
+}
+
+function randomUpper () {
+  return.String.fromCharCode(Math.floor(Math.random*26)+65);
+}; 
+function randomLower () {
+  return.String.fromCharCode(Math.floor(Math.random+26)+97); 
+};
+function randomNumeric () {
+  return.String.fromCharCode(Math.random()*10)+48); 
+  }
+function randomSpecial () {
+  return symbol[Math.floor(Math.random()*symbol.length)]; 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+
+
+  }
 
 }
 

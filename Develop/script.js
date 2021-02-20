23d04d867aa911913b0b2e4c1c1c12121e116660
Taylor Leong <tjl2125@alum.barnard.edu>
2021-02-20 01:42:10 -0500

    fixing minor errors and text errors

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,7 @@ generateBtn.addEventListener("click", writePassword);
 var generatePassword = function(){
 
   var password="";
-//prompting user to adjust length of their password
+  //prompting user to adjust length of their password
   var pLength = window.prompt("Please type in a number between 8-128 characters.");
   if (!pLength) {
     window.alert("Please select a number between 8-128 characters and try again.");
@@ -28,7 +28,7 @@ var generatePassword = function(){
     window.alert("Please select a number between 8-128 characters and try again.");
     return;
   }
-//checks to see if the input entered is actually a number
+  //checks to see if the input entered is actually a number
   else if(isNaN(pLength)){
     window.alert("Please type in a number not a character. Please try again");
     return;
@@ -36,38 +36,38 @@ var generatePassword = function(){
   else if (pLength >= 8 || pLength <= 128) {
     console.log(pLength);
   }
-//inserting the prompts for each of the choices
-  var lowerCasePrompt = confirm("Would you like to include lowercase letters in your password?");
-  console.log(lowerCase);
-  var upperCasePrompt = confirm("Would you like to include uppercase letters in your password?");
-  console.log(upperCase);
-  var numbersPrompt = confirm("Would you like to include numbers in your password?");
-  console.log(numbers);
-  var specialPrompt = confirm("Would you like to include special characters in your password?");
-  console.log(special); 
+  //inserting the prompts for each of the choices
+  var lowerCasePrompt = confirm("Do you want lower case letters in your password?");
+  
+  var upperCasePrompt = confirm("Do you want upper case letters in your password?");
+  
+  var numbersPrompt = confirm("Do you want numbers in your password?");
+ 
+  var specialPrompt = confirm("Do you want special characters in your password?");
+  
 
+  //prompting user to choose at least one choice
   if (lowerCasePrompt === false && upperCasePrompt === false && numbersPrompt === false && specialPrompt ===false) {
     window.alert ("Go back and choose one of the four options.");
     return; 
   } 
   else {
-      var combine = "";
+      var combination = "";
       if (lowerCasePrompt) {
-          combine = lowerCase; 
+          combination = lowerCase; 
       }
       if (upperCasePrompt) {
-          combine = combine + upperCase;
+          combination = combination + upperCase;
       }
       if (numbersPrompt) {
-          combine = combine + numbers;
+          combination = combination + numbers;
       }
       if (specialPrompt) {
-          combine = combine + special;
+          combination = combination + special;
       }
       for (var i = 0; i < pLength; i++) {
-          password += combine [Math.floor(Math.random() * combine.length)];
+          password += combination [Math.floor(Math.random() * combination.length)];
       }
   }
-
 return password;
 };
